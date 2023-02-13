659a528f861b2595737a272eb9aeabb485f901bd
Lin1404 <78375725+Lin1404@users.noreply.github.com>
2023-02-13 12:18:24 -0500

    Add Google Ads

--- frontend/src/home/index.jsx
+++ frontend/src/home/index.jsx
@@ -2,9 +2,25 @@ import Illustration from "./Illustration";
 import MeetingList from "./MeetingList";
 import "./index.css";
 
+const AdsByGoogle = () => {
+  (window.adsbygoogle = window.adsbygoogle || []).push({})
+  return (
+      <div>
+          <ins class="adsbygoogle"
+              style={{display:'block'}}
+              data-ad-client="ca-pub-2226955251055997"
+              data-ad-slot="7048770259"
+              data-ad-format="auto"
+              data-full-width-responsive="true">
+          </ins>
+      </div>
+  )
+}
+
 export default function Home() {
   return (
     <div className="container home-container">
+        <AdsByGoogle />
         <Illustration />
         <MeetingList />
     </div>
