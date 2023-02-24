8d4a7089216a918fcfe14d2dee65ca8cd1747c10
Lin1404 <78375725+Lin1404@users.noreply.github.com>
2023-02-24 16:20:40 -0500

    Add component for Google Ads

--- frontend/src/home/index.jsx
+++ frontend/src/home/index.jsx
@@ -1,11 +1,12 @@
 import Illustration from "./Illustration";
 import MeetingList from "./MeetingList";
+import AdsByGoogle from "./AdsByGoogle";
 import "./index.css";
 
-
-export default function Home() {
+export default function Home() {    
   return (
     <div className="container home-container">
+        <AdsByGoogle />
         <Illustration />
         <MeetingList />
     </div>
