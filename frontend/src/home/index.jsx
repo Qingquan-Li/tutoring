9d072120858a8e3b9abd2d6c6c3c72f4b27e096e
Lin1404 <78375725+Lin1404@users.noreply.github.com>
2023-02-24 14:29:53 -0500

    fix bug: Safari not support

--- frontend/src/home/index.jsx
+++ frontend/src/home/index.jsx
@@ -2,25 +2,10 @@ import Illustration from "./Illustration";
 import MeetingList from "./MeetingList";
 import "./index.css";
 
-const AdsByGoogle = () => {
-  (window.adsbygoogle = window.adsbygoogle || []).push({})
-  return (
-      <div>
-          <ins class="adsbygoogle"
-              style={{display:'block'}}
-              data-ad-client="ca-pub-5622524728195625"
-              data-ad-slot="7048770259"
-              data-ad-format="auto"
-              data-full-width-responsive="true">
-          </ins>
-      </div>
-  )
-}
 
 export default function Home() {
   return (
     <div className="container home-container">
-        <AdsByGoogle />
         <Illustration />
         <MeetingList />
     </div>
