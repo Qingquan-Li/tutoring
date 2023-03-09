c1b351fb6d8ae884d9fcb1bb9b66dfd28ff74cc8
Lin1404 <78375725+Lin1404@users.noreply.github.com>
2023-03-09 18:22:06 -0500

    use Production script scr for Google Ads

--- frontend/src/home/AdsByGoogle.jsx
+++ frontend/src/home/AdsByGoogle.jsx
@@ -5,10 +5,7 @@ const AdsByGoogle = () => {
     useEffect(()=>{
         const script = document.createElement('script');
         script.async = true;
-        // Test src
-        script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5622524728195625';
-        // Production scr
-        // script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2226955251055997'
+        script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2226955251055997'
         document.head.appendChild(script);
     
         (window.adsbygoogle = window.adsbygoogle || []).push({});
@@ -17,14 +14,14 @@ const AdsByGoogle = () => {
     return(
         <div>
             <script
-            data-ad-client = "ca-pub-5622524728195625"
+            data-ad-client = "ca-pub-2226955251055997"
             async
-            src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5622524728195625"
+            src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2226955251055997"
             />
             <ins 
                 className="adsbygoogle"
                 style={{display:'block'}}
-                data-ad-client="ca-pub-5622524728195625"
+                data-ad-client="ca-pub-2226955251055997"
                 data-ad-slot="7048770259"
                 data-ad-format="auto"
                 data-full-width-responsive="true">
